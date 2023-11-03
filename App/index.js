466790e440075f5d0adeee139cbea015d9a05a11
Adeitan Abimbola Moses <93311300+skimasax@users.noreply.github.com>
2023-11-03 16:37:56 +0100

    setup done

---
+++ App/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.listen(PORT, () => {
+    console.log(`started on port ${PORT}`);
+  });

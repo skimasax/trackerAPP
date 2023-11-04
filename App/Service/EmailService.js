002af726aa171e8e0ce9158ae8139c708979e4a8
Adeitan Abimbola Moses <93311300+skimasax@users.noreply.github.com>
2023-11-04 08:26:50 +0100

    updated

--- App/Service/EmailService.js
+++ App/Service/EmailService.js
@@ -10,25 +10,5 @@ const transporter = nodemailer.createTransport({
     },
   });
 
-
-
-//   const sendEmail = async (to, subject, text) => {
-//     try {
-//       const mailOptions = {
-//         from: 'bimbo@gmail.com', // Replace with the sender's email address
-//         to,
-//         subject,
-//         text,
-//       };
-  
-//       const info = await transporter.sendMail(mailOptions);
-//       console.log('Email sent: ' + info.response);
-//       return ('email sent successfully');
-//     } catch (error) {
-//       console.error('Error sending email: ' + error);
-//       throw new Error('Error sending email');
-//     }
-//   };
-
 module.exports = transporter;
   
